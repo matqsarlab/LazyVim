1dbfb75740469e135e95eb587c4a3268b27fde2e
matqsarlab <m.balicki@qsarlab.com>
2024-11-07 00:36:23 +0100

    first

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  opts = {
+    -- use the night style
+    style = "moon",
+    -- transparent
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+    },
+    on_colors = function(colors)
+      colors.bg_visual = "#816b9c"
+    end,
+  },
+}
---
+++ lua/plugins/indent.lua
@@ -0,0 +1,35 @@
+return {
+  "echasnovski/mini.indentscope",
+  version = false, -- wait till new 0.7.0 release to put it back on semver
+  event = "LazyFile",
+  opts = {
+    -- symbol = "▏",
+    symbol = "┃",
+    options = { try_as_border = true },
+    draw = {
+      -- Delay (in ms) between event and start of drawing scope indicator
+      delay = 100
+    }
+  },
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "alpha",
+        "dashboard",
+        "fzf",
+        "help",
+        "lazy",
+        "lazyterm",
+        "mason",
+        "neo-tree",
+        "notify",
+        "toggleterm",
+        "Trouble",
+        "trouble",
+      },
+      callback = function()
+        vim.b.miniindentscope_disable = true
+      end,
+    })
+  end,
+}
