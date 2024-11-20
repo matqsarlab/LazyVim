bc5acec7597950ef38fbc447c332fc55f23e3efc
matqsarlab <m.balicki@qsarlab.com>
2024-11-20 11:07:18 +0100

    Add trasparencity

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,6 +8,7 @@ return {
     transparent = true,
     styles = {
       sidebars = "transparent",
+      floats = "transparent",
     },
     on_colors = function(colors)
       colors.bg_visual = "#816b9c"
